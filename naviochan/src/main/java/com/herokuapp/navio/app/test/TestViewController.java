f6b4a09d05b7a6c92eeae8e5be72aa7cbb7c25c8
kkiku324 <65221512+kkiku324@users.noreply.github.com>
2020-05-13 11:15:46 +0900

    only show top page

---
+++ naviochan/src/main/java/com/herokuapp/navio/app/test/TestViewController.java
@@ -0,0 +1,39 @@
+package com.herokuapp.navio.app.test;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//import com.herokuapp.navio.entity.WordData;
+
+@Controller
+@RequestMapping("/test")
+public class TestViewController {
+
+	@GetMapping("/")
+	public String index() {
+		return "test/top";
+	}
+	
+//	@PostMapping("/generate")
+//	public String generate(@Validated TestViewForm testViewForm, BindingResult result, Model model) {
+//		if (result.hasErrors() || testViewForm.getEndPosition() - testViewForm.getStartPosition() < 50) {
+//			model.addAttribute("errorMessage", "1～1900の範囲で入力してください。");
+//			
+//			return "test/top";
+//		}
+//		
+//		// 出題する問題のデータを取得する
+//		List<WordData> wordDataList = TestViewLogic.selectWords(testViewForm.getEndPosition(), testViewForm.getEndPosition());
+//
+//		model.addAttribute("wordDataList", wordDataList);
+//		
+//		return "test/paper";
+//	}
+}
